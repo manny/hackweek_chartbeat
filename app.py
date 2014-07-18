af70f6c04d3ea4b5e8f4182fa31a513042a93b9a
Manny Lopez <manny.lopez625@gmail.com>
2014-07-18 13:15:43 -0400

    twilio boilerplate

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from twilio.rest import TwilioRestClient
+from token import auth_token, account_sid
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "yo dawg"
+@app.route('/<path:url>', methods=['get'])
+def home(url):
+    client = TwilioRestClient(account_sid, auth_token)
+
+    client.messages.create(body=url,
+        to="+18565582697",
+        from_="+18564578834")
+    return "only got " + url
 
 if __name__ == '__main__':
     app.run()
